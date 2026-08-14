c885d92c61d3e8154f103105d1f384a49088a92d
hebaalmarema1-hub <hebaalmarema1@gmail.com>
2026-08-14 22:31:31 +0200

    Add Episode02 composition to RemotionRoot

--- Root.tsx
+++ Root.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Composition } from "remotion";
 import { Intro } from "./components/Intro";
 import { Episode01 } from "./episodes/episode01/Episode01";
+import { Episode02 } from "./episodes/episode02/Episode02";
+
 export const RemotionRoot: React.FC = () => {
   return (
     <>
@@ -13,10 +15,20 @@ export const RemotionRoot: React.FC = () => {
         width={1920}
         height={1080}
       />
+
       <Composition
         id="Episode01"
         component={Episode01}
         durationInFrames={4260}
+        fps={30}
+        width={1920}
+        height={1080}
+      />
+
+      <Composition
+        id="Episode02"
+        component={Episode02}
+        durationInFrames={6930}
         fps={30}
         width={1920}
         height={1080}
